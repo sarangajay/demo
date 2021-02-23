61f713ddad3f186e4615092cb660e518c2632c2a
sarangajay <Oneli2008>
2021-02-23 21:23:38 +0100

    non compiling totalOfSingleItemCart() test to demonstrate that we only write source code when only it is required by a test.

--- src/test/java/com/promotion/PromotionEngineTest.java
+++ src/test/java/com/promotion/PromotionEngineTest.java
@@ -2,6 +2,8 @@ package com.promotion;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PromotionEngineTest {
@@ -10,5 +12,10 @@ public class PromotionEngineTest {
     public void totalOfEmptyCart(){
         ShoppingCart cart = new ShoppingCart();
         assertEquals(0.0, cart.getTotal());
+    }
+
+    @Test
+    public void totalOfSingleItemCart() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(new Item()));
     }
 }
