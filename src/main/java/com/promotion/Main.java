b114c013985b6c9cc03b621e564c857649b36821
sarangajay <shenali@Shenalis-MacBook-Pro.local>
2021-02-23 19:02:52 +0100

    init project with junit setup

---
+++ src/main/java/com/promotion/Main.java
@@ -0,0 +1,7 @@
+package com.promotion;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hi");
+    }
+}
---
+++ src/test/java/com/promotion/JunitSetupTest.java
@@ -0,0 +1,13 @@
+package com.promotion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JunitSetupTest {
+
+    @Test
+    public void junitSetupTest(){
+         assertEquals(1,2);
+    }
+}
